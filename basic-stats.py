e5bc4b7ea0749af6f42ef7770966a3657e7fe894
john <jhancock1975@gmail.com>
2017-01-22 11:47:35 -0500

    update gitignore and add code for computing linear regression slope and intercept and correlation

--- basic-stats.py
+++ basic-stats.py
@@ -14,3 +14,33 @@ def plusMinusQty(l):
   return factor(l)*(( variance(l)/len(l) ) ** 0.5)
 
 flatten = lambda l: [item for sublist in l for item in sublist]
+
+def getComponent(l, index):
+  return [ l[i][index] for i in range(len(l)) ]
+
+def xValues(l):
+  return getComponent(l, 0)
+
+def yValues(l):
+  return getComponent(l, 1)
+
+def linRegSlope(l):
+  xList = xValues(l)
+  yList = yValues(l)
+  meanX = mean(xList)
+  meanY = mean(yList)  
+  return sum( [ (xList[i] - meanX) * (yList[i] - meanY) for i in range(len(l))] ) / sum( [ (xList[i] - meanX)**2 for i in range(len(l)) ] )
+
+def linRegIntercept(l):
+  xList = xValues(l)
+  yList = yValues(l)
+  meanX = mean(xList)
+  meanY = mean(yList)  
+  return meanY - linRegSlope(l)*meanX
+
+def correlation(l):
+  xList = xValues(l)
+  yList = yValues(l)
+  meanX = mean(xList)
+  meanY = mean(yList)  
+  return sum( [ (xList[i] - meanX) * (yList[i] - meanY) for i in range(len(l))] ) / ( sum( [ (xList[i] - meanX)**2 for i in range(len(l)) ] ) * sum( [ (yList[i] - meanY)**2 for i in range(len(l)) ] )  )**0.5
